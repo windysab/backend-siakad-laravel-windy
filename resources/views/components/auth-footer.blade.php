1b6acd5a4bb950f5e59a05c042715274906ce02a
windys <windysabtami85@gmail.com>
2023-10-11 21:35:52 +0800

    pages aut and home ready

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+<div class="simple-footer">
+    Copyright &copy; WindySabtam, S.Kom 2023
+</div>
---
+++ resources/views/components/auth-header.blade.php
@@ -0,0 +1,3 @@
+<div class="login-brand">
+    <img src="{{ asset('img/logopta.png') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+</div>
